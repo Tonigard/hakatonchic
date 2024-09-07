d55a09234e8ad6133347456c1cf0911d9bde1e51
Nickita Rusanov <nipe322@gmail.com>
2024-09-07 15:09:32 +0300

    Add path validation

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
 import sys
 import utils.args_list as args_list
+from pathlib import Path
 
-def main(interval: str):
-    print(interval)
+def main(data: Path):
+    print(data)
     
 
 if __name__ == '__main__':
     interval = '-t3'
 
     if any(flag in sys.argv for flag in args_list.HELP_FLAGS):
-        print('Это справка по работу с данной утилитой')
+        print(
+            '\nДля генерации предсказаний модели укажите путь к файлу с данными в формате csv',
+            'Пример запусука python3 main.py [file_path]\n',
+            'Список аргументов для настройки запуска утилиты:',
+            '--tune [file_path] - дообучения модели на данных предоставленых в файле file_path',
+            sep='\n'
+        )
         exit(0)
-    if any(flag in sys.argv for flag in args_list.PREDICTION_INTERVAL_FLAGS):
-        interval_args = [flag for flag in sys.argv if flag in args_list.PREDICTION_INTERVAL_FLAGS]
-        interval = interval_args[0]
-        for arg in interval_args:
-            sys.argv.remove(arg)
+    if any(flag in sys.argv for flag in args_list.TUNING_FLAGS):
+        pass
+    if pred_data := Path(sys.argv[1]):
+        if not pred_data.exists():
+            print('Неверный путь к файлу')
+            exit(1)
+        main(pred_data)
+        exit(0)
     if sys.argv[1:]:
         print('Неизвестные аргументы:', *sys.argv[1:])
         exit(1)
 
-    main(
-        interval=interval
-    )
+    
--- utils/args_list.py
+++ utils/args_list.py
@@ -1,2 +1,2 @@
 HELP_FLAGS = ('--help', '-h')
-PREDICTION_INTERVAL_FLAGS = ('-t3', '-t6', '-t9', '-t12')
+TUNING_FLAGS = ('--tune')
